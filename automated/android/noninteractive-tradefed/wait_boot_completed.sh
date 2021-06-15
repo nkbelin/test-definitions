0e9a760048a85cafc6f948a028a335d8bb1ec436
Nicolas Belin <nbelin@baylibre.com>
2021-06-15 19:08:28 +0200

    Fix the fact that no result regarding boot completed is created
    Signed-off-by: Nicolas Belin <nbelin@baylibre.com>

--- automated/android/noninteractive-tradefed/wait_boot_completed.sh
+++ automated/android/noninteractive-tradefed/wait_boot_completed.sh
@@ -7,51 +7,40 @@
 
 export PATH=$PWD/platform-tools:$PATH
 TIMEOUT="300"
-TEST_URL="http://testdata.validation.linaro.org/cts/android-cts-7.1_r1.zip"
-TEST_PARAMS="cts -m CtsBionicTestCases --abi arm64-v8a --disable-reboot --skip-preconditions --skip-device-info"
-TEST_PATH="android-cts"
-RESULT_FORMAT="aggregated"
-RESULT_FILE="$(pwd)/output/result.txt"
-export RESULT_FILE
-# the default number of failed test cases to be printed
-FAILURES_PRINTED="0"
-# WIFI AP SSID
-AP_SSID=""
-# WIFI AP KEY
-AP_KEY=""
-DURATION="0"
 
 usage() {
-    echo "Usage: $0 [-o timeout] [-n serialno] [-c cts_url] [-t test_params] [-p test_path] [-r <aggregated|atomic>] [-f failures_printed] [-a <ap_ssid>] [-k <ap_key>]" 1>&2
+    echo "Usage: $0 [-o timeout] [-n serialno]" 1>&2
     exit 1
 }
 
-while getopts ':o:n:c:t:p:r:f:a:k:d:' opt; do
+while getopts ':o:n:' opt; do
     case "${opt}" in
         o) TIMEOUT="${OPTARG}" ;;
         n) export ANDROID_SERIAL="${OPTARG}" ;;
-        c) TEST_URL="${OPTARG}" ;;
-        t) TEST_PARAMS="${OPTARG}" ;;
-        p) TEST_PATH="${OPTARG}" ;;
-        r) RESULT_FORMAT="${OPTARG}" ;;
-        f) FAILURES_PRINTED="${OPTARG}" ;;
-        a) AP_SSID="${OPTARG}" ;;
-        k) AP_KEY="${OPTARG}" ;;
-        d) DURATION="${OPTARG}" ;;
         *) usage ;;
     esac
 done
 
-if [ -e "/home/testuser" ]; then
-    export HOME=/home/testuser
-fi
+timeout="${TIMEOUT}"
+# shellcheck disable=SC2039
+end=$(( $(date +%s) + timeout ))
 
-wait_boot_completed_fatal "${TIMEOUT}"
+# shellcheck disable=SC2039
+boot_completed=false
+while [ "$(date +%s)" -lt "$end" ]; do
+    if adb shell getprop sys.boot_completed | grep "1"; then
+        boot_completed=true
+        break
+    else
+        sleep 3
+    fi
+done
 
-if [ $? -eq 0 ]; then
-    lava-test-case "wait_booted" --result "pass"
+if "${boot_completed}"; then
     info_msg "Target booted up completely."
+    exit 0
 else
     lava-test-case "wait_booted" --result "fail"
-    error_fatal "wait_boot_completed timed out."
+    info_msg "wait_boot_completed timed out."
+    exit 1
 fi
